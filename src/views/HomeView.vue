ca98538a4ffd270725b88917683661ef19342b75
WangMr <1308860052@qq.com>
2023-04-04 17:02:26 +0800

    初始化了项目

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  我是来练习分支的
+  </div>
+</template>
+<script  setup>
+import { useRoute, useRouter } from 'vue-router';
+import { ref, reactive, onMounted, } from 'vue';
+</script>
+<style lang='scss' scoped>
+ 
+</style>
